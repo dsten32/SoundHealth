e3f4bd1eaba36626676bbfaf9058b718bf52a6f3
Darcy Cowan <dsten32@gmail.com>
2019-05-21 20:51:29 +1200

    added DataDao

---
+++ app/src/main/java/com/comp576/soundhealth/DataDao.java
@@ -0,0 +1,22 @@
+package com.comp576.soundhealth;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface DataDao {
+    @Insert
+    void insert(Data data);
+    @Update
+    void update(Data data);
+    @Delete
+    void delete(Data data);
+    @Query("SELECT * from Data ORDER BY id")
+    LiveData<List<Data>> getAllData();
+}
