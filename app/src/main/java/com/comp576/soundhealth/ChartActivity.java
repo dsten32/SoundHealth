48335f80a66a5ff48ee59fd34f6563c2f458fc22
Darcy Cowan <dsten32@gmail.com>
2019-06-29 14:58:32 +1200

    working on barchart. need to figure out how to organise data by date, and then by dB value to add to columns and subcolumns for chart. ideally chart should extent outside of screen and be scrollable.

--- app/src/main/java/com/comp576/soundhealth/ChartActivity.java
+++ app/src/main/java/com/comp576/soundhealth/ChartActivity.java
@@ -1,63 +1,56 @@
 package com.comp576.soundhealth;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.appcompat.widget.Toolbar;
 import lecho.lib.hellocharts.model.*;
 import lecho.lib.hellocharts.model.SliceValue;
 import lecho.lib.hellocharts.model.ColumnChartData;
 import lecho.lib.hellocharts.model.Column;
 import lecho.lib.hellocharts.model.SubcolumnValue;
-//import lecho.lib.hellocharts.model.*;
 import lecho.lib.hellocharts.view.PieChartView;
 import lecho.lib.hellocharts.listener.PieChartOnValueSelectListener;
-import lecho.lib.hellocharts.view.ColumnChartView;
 import lecho.lib.hellocharts.listener.ColumnChartOnValueSelectListener;
+import lecho.lib.hellocharts.view.ColumnChartView;
 
-public class ChartActivity extends AppCompatActivity{
+public class ChartActivity extends AppCompatActivity {
     private PieChartView pieChartView;
     private ColumnChartView barChartView;
     private ArrayAdapter<Data> adapter;
     private List<Data> data = new ArrayList<>();
     private ListView dataListView;
     private DataRepository dataRepository;
-    private Boolean dataQueried=false;
-    private float thirties,fourties,fifties,sixties,seventies,eighties,nintiesPlus;
+    private Boolean dataQueried = false;
+    private float thirties, fourties, fifties, sixties, seventies, eighties, nintiesPlus;
     private int dataTotal;
 
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Sound Chart");
+
         setContentView(R.layout.activity_chart);
         dataRepository = new DataRepository(this);
 
         pieChartView = findViewById(R.id.pieChart);
         barChartView = findViewById(R.id.barChart);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Sound Chart");
 
 //get user's data for display. may want to create a dB class to only grab that info.
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
                 data.addAll(dataRepository.getDataList());
-                dataQueried=true;
+                dataQueried = true;
             }
         });
 
@@ -68,30 +61,30 @@ public class ChartActivity extends AppCompatActivity{
             e.printStackTrace();
         }
 
-        dataTotal=data.size();
+        dataTotal = data.size();
 
 //Generating slice data, simple categorisation of dB levels and taking percents.
-        for(Data dataPoint :data) {
+        for (Data dataPoint : data) {
             Double dB = dataPoint.dB;
             //check if null in case I forget to enable mic.
             if (dB != null) {
                 float addPercent = 100 / dataTotal;
-            if (dB > 90) {
-                nintiesPlus += addPercent;
-            } else if (dB > 80) {
-                eighties += addPercent;
-            } else if (dB > 70) {
-                seventies += addPercent;
-            } else if (dB > 60) {
-                sixties++;
-            } else if (dB > 50) {
-                fifties += addPercent;
-            } else if (dB > 40) {
-                fourties += addPercent;
-            } else if (dB > 30) {
-                thirties += addPercent;
+                if (dB > 90) {
+                    nintiesPlus += addPercent;
+                } else if (dB > 80) {
+                    eighties += addPercent;
+                } else if (dB > 70) {
+                    seventies += addPercent;
+                } else if (dB > 60) {
+                    sixties++;
+                } else if (dB > 50) {
+                    fifties += addPercent;
+                } else if (dB > 40) {
+                    fourties += addPercent;
+                } else if (dB > 30) {
+                    thirties += addPercent;
+                }
             }
-        }
         }
 
         List pieData = new ArrayList<>();
@@ -108,57 +101,85 @@ public class ChartActivity extends AppCompatActivity{
         pieChartData.setHasCenterCircle(true).setCenterCircleScale(0.7f).setCenterText1("Your Sound Profile").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
         pieChartView.setPieChartData(pieChartData);
 
-        pieChartView.setOnValueTouchListener(new ValueTouchListener(pieChartData,pieChartView));
+        pieChartView.setOnValueTouchListener(new ValueTouchListener(pieChartData, pieChartView));
         pieChartView.startDataAnimation();
 
-
         //users daily chart, TODO
-        List barData = new ArrayList<>();
+        int numColumns = 7;
 
-        ColumnChartData barChartData = new ColumnChartData(barData);
+        List<Column> columns = new ArrayList<>();
+        List<SubcolumnValue> values;
+
+        for (int i = 0; i < numColumns; i++) {
+            values = new ArrayList<>();
+//            for(int j=0;j<7;j++){
+            //placeholder subcolumn value for testing, need to split data by type and add colours and labels.
+            values.add(new SubcolumnValue(thirties, Color.GREEN));
+            values.add(new SubcolumnValue(fourties, Color.BLUE));
+            values.add(new SubcolumnValue(fifties, Color.CYAN));
+            values.add(new SubcolumnValue(sixties, Color.GRAY));
+            values.add(new SubcolumnValue(seventies, Color.YELLOW));
+            values.add(new SubcolumnValue(eighties, Color.MAGENTA));
+            values.add(new SubcolumnValue(nintiesPlus, Color.RED));
+//            }
+            Column column = new Column(values);
+            column.setHasLabels(true);
+            columns.add(column);
+        }
+
+        ColumnChartData barChartData = new ColumnChartData(columns);
+        barChartData.setStacked(true);
+
+        barChartView.setColumnChartData(barChartData);
+        barChartView.setHorizontalScrollBarEnabled(true);
+        barChartView.setOnValueTouchListener(new ColumnChartOnValueSelectListener() {
+            @Override
+            public void onValueSelected(int columnIndex, int subcolumnIndex, SubcolumnValue value) {
+
+            }
+
+            @Override
+            public void onValueDeselected() {
+
+            }
+        });
     }
 
     @Override
-    public boolean onSupportNavigateUp(){
+    public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
-    public void onEnterAnimationComplete(){
-        if(data.isEmpty()){
-            Toast.makeText(this,dataQueried.toString(),Toast.LENGTH_SHORT).show();
 
+    public void onEnterAnimationComplete() {
+        if (data.isEmpty()) {
+            Toast.makeText(this, dataQueried.toString(), Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "#datapoints collected: "+ data.size(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "#datapoints collected: " + data.size(), Toast.LENGTH_SHORT).show();
         }
     }
 
-    private class ValueTouchListener implements PieChartOnValueSelectListener{
+    private class ValueTouchListener implements PieChartOnValueSelectListener {
         private PieChartView pieChartView;
         private PieChartData pieChartData;
 
-        public ValueTouchListener(PieChartData pieChartData, PieChartView pieChartView){
-            this.pieChartData=pieChartData;
-            this.pieChartView=pieChartView;
+        public ValueTouchListener(PieChartData pieChartData, PieChartView pieChartView) {
+            this.pieChartData = pieChartData;
+            this.pieChartView = pieChartView;
         }
-
 
         @Override
         public void onValueSelected(int arcIndex, SliceValue value) {
-
-            pieChartData.setHasCenterCircle(true).setCenterCircleScale(0.7f).setCenterText1("Your exposure to "+String.valueOf(value.getLabelAsChars())+" was").setCenterText1FontSize(15).setCenterText1Color(Color.parseColor("#0097A7")).setCenterText2(String.valueOf(Math.round(value.getValue()))+"%").setCenterText2FontSize(10).setCenterText2Color(Color.parseColor("#0097A7"));
-//            pieChartData.setHasCenterCircle(true)
-
+            pieChartData.setHasCenterCircle(true).setCenterCircleScale(0.7f).setCenterText1("Your exposure to " + String.valueOf(value.getLabelAsChars()) + " was").setCenterText1FontSize(15).setCenterText1Color(Color.parseColor("#0097A7")).setCenterText2(String.valueOf(Math.round(value.getValue())) + "%").setCenterText2FontSize(10).setCenterText2Color(Color.parseColor("#0097A7"));
             pieChartView.setPieChartData(pieChartData);
-            Toast.makeText(getApplication().getApplicationContext(),"db Range: "+String.valueOf(value.getLabelAsChars()),Toast.LENGTH_SHORT).show();
-
-            }
+            Toast.makeText(getApplication().getApplicationContext(), "db Range: " + String.valueOf(value.getLabelAsChars()), Toast.LENGTH_SHORT).show();
+        }
 
         @Override
         public void onValueDeselected() {
-                pieChartData.setHasCenterCircle(true).setCenterText1("Your Sound Profile").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
-                pieChartView.setPieChartData(pieChartData);
-
-            }
+            pieChartData.setHasCenterCircle(true).setCenterText1("Your Sound Profile").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
+            pieChartView.setPieChartData(pieChartData);
+        }
     }
 
 }
--- app/src/main/java/com/comp576/soundhealth/Recorder.java
+++ app/src/main/java/com/comp576/soundhealth/Recorder.java
@@ -16,22 +16,21 @@ import static androidx.core.content.ContextCompat.checkSelfPermission;
 https://stackoverflow.com/questions/10655703/what-does-androids-getmaxamplitude-function-for-the-mediarecorder-actually-gi
 user https://stackoverflow.com/users/806920/lukas-ruge
 */
-public class Recorder{
+public class Recorder {
 
     private final String TAG = "Recorder Function: ";
     public static double REFERENCE = 0.00002;
 
-    public double getNoiseLevel() throws NoValidNoiseLevelException
-    {
+    public double getNoiseLevel() throws NoValidNoiseLevelException {
 
         Log.i(TAG, "start new recording process");
-        int bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_DEFAULT,AudioFormat.ENCODING_PCM_16BIT);
+        int bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
         //making the buffer bigger....
-        bufferSize=bufferSize*4;
+        bufferSize = bufferSize * 4;
         AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
                 44100, AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
 
-        short data [] = new short[bufferSize];
+        short data[] = new short[bufferSize];
         double average = 0.0;
         recorder.startRecording();
         //recording data;
@@ -39,34 +38,31 @@ public class Recorder{
 
         recorder.stop();
         Log.i(TAG, "stop");
-        for (short s : data)
-        {
-            if(s>0)
-            {
+        for (short s : data) {
+            if (s > 0) {
                 average += Math.abs(s);
-            }
-            else
-            {
+            } else {
                 bufferSize--;
             }
         }
+        data=null;
         //x=max;
-        double x = average/bufferSize;
-        Log.i(TAG, ""+x);
+        double x = average / bufferSize;
+        Log.i(TAG, "" + x);
         recorder.release();
         Log.i(TAG, "getNoiseLevel() ");
-        double db=0;
-        if (x==0){
+        double db = 0;
+        if (x == 0) {
             throw new NoValidNoiseLevelException(x);
         }
+
         // calculating the pascal pressure based on the idea that the max amplitude (between 0 and 32767) is
         // relative to the pressure
-        double pressure = x/51805.5336; //the value 51805.5336 can be derived from assuming that x=32767=0.6325 Pa and x=1 = 0.00002 Pa (the reference value)
-        Log.i(TAG, "x="+pressure +" Pa");
-        db = (20 * Math.log10(pressure/REFERENCE));
-        Log.i(TAG, "db="+db);
-        if(db>0)
-        {
+        double pressure = x / 51805.5336; //the value 51805.5336 can be derived from assuming that x=32767=0.6325 Pa and x=1 = 0.00002 Pa (the reference value)
+        Log.i(TAG, "x=" + pressure + " Pa");
+        db = (20 * Math.log10(pressure / REFERENCE));
+        Log.i(TAG, "db=" + db);
+        if (db > 0) {
             return db;
         }
         throw new NoValidNoiseLevelException(x);
